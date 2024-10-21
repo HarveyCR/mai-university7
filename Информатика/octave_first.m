3d1a8f8fdb0d5e90f20a94fdccf2fcc628790d61
Harvey <lbf.asus.k00z.2014@gmail.com>
2024-10-21 19:41:39 +0300

    Лекция + Задание для Лаб

---
+++ Информатика/octave_first.m
@@ -0,0 +1,6 @@
+C = cell(1,4)
+C{1} = ["Mihailov"; "Afanasiyvich"; "Bulgakov"]
+C{2} = [15 05 1891, 10 03 1940]
+C{3} = char("BG", "M&M", "DT", "Gospodin iz San_francisco")
+C{4} = logical([1;1;0;0;1])
+C{3}(C{4})
