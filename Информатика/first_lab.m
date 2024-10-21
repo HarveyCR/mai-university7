9bc99217d8576810c4cc86c6800593cf1c12292a
HarveyCR <lbf.asus.k00z.2014@gmail.com>
2024-10-21 20:31:50 +0300

    Лабораторная работа

--- Информатика/first_lab.m
+++ Информатика/first_lab.m
@@ -3,7 +3,7 @@ y = -0.825;
 z = 0.160;
 a = (1 + sinh(x+y))/abs(x - (2*y / 1 + x.^2 * y.^2))
 b = cos(atan(1) / z)^2
-clear x, y, z, a, b
+clear x, y, z, a, b;
 
 #x = 2 - 4 * rand(1, 1e6);
 #y = 2 - 4 * rand(1, 1e6);
