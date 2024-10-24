e436f92438548ebce8e42baff5aaee8c8aaa9e71
Harvey <lbf.asus.k00z.2014@gmail.com>
2024-10-24 22:24:31 +0300

    лекция кривилева

--- Информатика/Лекции/lecture_2.m
+++ Информатика/Лекции/lecture_2.m
@@ -1,7 +1,32 @@
-A = [1 2 3];
-format compact;
-whos A;
-A = vertcat(1,2,3);
-size(A, 2);
+#A = [1 2 3];
+#format compact;
+#whos A;
+#A = vertcat(1,2,3);
+#size(A, 2);
 
-A = rand(3, 3)
+#A = rand(1, 3);
+#sqrt(sum(A.^2));
+#norm(A, 2);
+#B = - rand(1, 3)
+#dot(A, B)
+#lenA = norm(A)
+#lenB = norm(A)
+#angle = dot(A,B) / (lenA * lenB)
+#acosd(angle)
+#cos(angle) * 180/pi
+
+#A = [6 3; 1 6]
+#B = [7 5; 5 2]
+
+#A + B
+#A - B
+#A.*B
+#[A B A*B A.*B]
+#det(A) # Определитель
+#A' * A
+#rank(A)
+
+A = [3 -7 7; 1 -8 9; 4 -7 14]
+B = [-8; -15; -13]
+
+Xm = inv(A) * B
