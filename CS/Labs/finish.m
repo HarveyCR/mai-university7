eadaba12b5f00a9bad7add73a25447024c728995
Harvey <lbf.asus.k00z.2014@gmail.com>
2024-11-18 22:30:03 +0300

    new_start_files

--- CS/Labs/finish.m
+++ CS/Labs/finish.m
@@ -1,10 +1,3 @@
-[iconData, iconCmap] = imread('grfwnd','ico');
-createMode.WindowStyle = 'modal';
-createMode.Interpreter = 'tex';
-message = {'Вы вошли в Мир';'\bfMat^{rix} Lab^{oratory}'};
-msgbox(message,'Привет!','custom', iconData, iconCmap, createMode);
-clear message iconData iconCmap createMode
-
 quest = {'Вы действительно хотите покинуть мир','\bfMat^{rix} Lab^{oratory}?'};
 Options.Default = 'Нет'; Options.Interpreter = 'tex'; l = false;
 l = strcmp(questdlg(quest,'Выход','До свидания', 'Нет', Options),'До свидания');
