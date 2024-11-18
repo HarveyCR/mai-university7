93c1fa503472650b68a7dbf039c92094a85193af
HarveyCR <lbf.asus.k00z.2014@gmail.com>
2024-11-18 22:13:23 +0300

    lab

--- CS/Labs/lab_3_3.m
+++ CS/Labs/lab_3_3.m
@@ -1,11 +1,6 @@
-[iconData, iconCmap] = imread('grfwnd','ico');
-createMode.WindowStyle = 'modal';
-createMode.Interpreter = 'tex';
-message = {' Вы вошли в мир';'\bfMat^{rix} Lab^{oratory}'};
-msgbox(message,'Привет!','custom',iconData,iconCmap,createMode);
-screen= get(0,'screensize');
-width=450; height=350;
-position=[fix((screen(3)-width)/2), fix((screen(4)-height)/2), width, height];
+screen = get(0,'Screensize');
+width = 450; height = 350;
+position=[fix((screen(3)-width)/2),...
+  fix((screen(4)-height)/2),width,height];
 set(0,'DefaultFigurePosition', position);
 clear screen position width height
-clear message iconData iconCmap createMode
