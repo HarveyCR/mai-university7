65f10d58528eac8deccc61dd63a69602ceb4599e
Harvey <lbf.asus.k00z.2014@gmail.com>
2025-06-12 13:59:18 +0300

    matlab

---
+++ CS/matlab/code/SDO.m
@@ -0,0 +1,7 @@
+params1 = 5;
+params2 = 2;
+
+round(11*params1 - 4*params1*params1 - 6, 3)
+round(-2*(5*params1 - 2*params1*params1-2), 3)
+round(-8*(2-params1)^2, 3)
+round(-7*params1 - 2*params1^2 - 6, 3)
