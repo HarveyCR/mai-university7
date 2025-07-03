a9a9134d37e5de6b57253eebb3d2abf7b6b18d69
Harvey <lbf.asus.k00z.2014@gmail.com>
2025-07-03 09:54:06 +0300

    Ball flight

---
+++ CS/matlab/code/ball_flight.m
@@ -0,0 +1,17 @@
+function [x, y, flight_time, maximum_height, flight_range] = ball_flight(initial_speed, angle)
+    g = 9.81;
+    flight_time = 2 * initial_speed * sin(deg2rad(angle)) / g;
+    maximum_height = initial_speed ^2 * sin(deg2rad(angle))^2 / (2*g);
+    flight_range = initial_speed ^2 * sin(deg2rad(2 * angle)) / g;
+
+    t = linspace(0, flight_time, 100);
+    x = initial_speed * cos(deg2rad(angle)) * t;
+    y = initial_speed * sin(deg2rad(angle)) .* t - 0.5 * g * t.^2;
+    
+    figure;
+    plot(x, y, 'LineWidth', 2);
+    title('Траектория полёта');
+    xlabel('Горизонтальное смещение, м');
+    ylabel('Высота, м');
+    grid on;
+end
