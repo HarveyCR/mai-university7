e52d2f4ea38ae8c50aa81630009792fe0e9f7571
Harvey <lbf.asus.k00z.2014@gmail.com>
2025-10-15 14:08:40 +0300

    Вентиль  Гайка

--- IT/Python_Labs/sql_test.py
+++ IT/Python_Labs/sql_test.py
@@ -1,30 +1,7 @@
-import mysql.connector
+def main():
+    a = {2, 1, 3}
+    print(2 in a)
 
-# Настройки подключения к базе данных (замени своими)
-config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "MmSs$40974097",
-    "database": "jokes_database"
-}
 
-# Читаем SQL-запрос из файла joke.sql
-with open("C:\\mai-university7\\IT\\Python_Labs\\joke.sql", "r", encoding="utf-8") as f:
-    sql_query = f.read().format(id=2)
-
-connection = mysql.connector.connect(**config)
-cursor = connection.cursor()
-
-cursor.execute(sql_query)
-
-result = cursor.fetchone()
-print(result)
-# Если в запросе возвращается одна колонка (например, текст шутки)
-if result:
-    joke = result[0]
-    print("Шутка:", joke)
-else:
-    print("Шутка не найдена")
-
-cursor.close()
-connection.close()
+if __name__ == '__main__':
+    main()
